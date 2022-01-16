c9f97b455335809ddeaf8a63161401ab26941ee7
MeteorTony <meteormeteor1028@gmail.com>
2022-01-16 14:09:00 +0800

    2nd version(more notes + better audio)

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
-const WHITE_KEYS = ["z", "x", "c", "v", "b", "n", "m"];
-const BLACK_KEYS = ["s", "d", "g", "h", "j"];
+const WHITE_KEYS = ["z", "x", "c", "v", "b", "n", "m", ",", ".", "/"];
+const BLACK_KEYS = ["s", "d", "g", "h", "j", "l", ";"];
 
 const keys = document.querySelectorAll(".key");
 const whiteKeys = document.querySelectorAll(".key.white");
 const blackKeys = document.querySelectorAll(".key.black");
+
+var interval = 1000; // larger interval -> longer extension of note(with pedal)
 
 keys.forEach((key) => {
   key.addEventListener("click", () => playNote(key));
 });
 
 document.addEventListener("keydown", (e) => {
-  if (e.repeat) return;                     // if pressing a key -> won't keep repeating
+  if (e.repeat) return;
+  //return; // if pressing a key -> won't keep repeating
   const key = e.key;
   const whiteKeyIndex = WHITE_KEYS.indexOf(key);
   const blackKeyIndex = BLACK_KEYS.indexOf(key);
@@ -19,12 +22,30 @@ document.addEventListener("keydown", (e) => {
   if (blackKeyIndex > -1) playNote(blackKeys[blackKeyIndex]);
 });
 
+function fadeOut(key) {
+  const noteAudio = document.getElementById(key.dataset.note);
+
+  var fadeAudio = setInterval(() => {
+    if (noteAudio.volume >= 0.0) {
+      if (noteAudio.volume - 0.01 < 0) {
+        noteAudio.volume = 0.0;
+        clearInterval(fadeAudio);
+      } else {
+        noteAudio.volume -= 0.01;
+      }
+    }
+  }, interval);
+  key.classList.remove("active");
+}
+
 function playNote(key) {
   const noteAudio = document.getElementById(key.dataset.note);
-  noteAudio.currentTime = 0;                // can immediately restart if click multiple times
+  noteAudio.currentTime = 0; // can immediately restart if click multiple times
+  noteAudio.volume = 1;
   noteAudio.play();
   key.classList.add("active");
-  noteAudio.addEventListener("ended", () => {   // remove active class
+  document.addEventListener("keyup", () => fadeOut(key));
+  noteAudio.addEventListener("ended", () => {
     key.classList.remove("active");
   });
 }
